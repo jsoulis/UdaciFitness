a07eeae97973906c96bb38e0c00e13903b8434ce
jsoulis <jtsoulis7@gmail.com>
2020-03-31 22:34:07 +0900

    Implmemented parts of the AddEntry component

--- App.js
+++ App.js
@@ -1,11 +1,19 @@
-import React from 'react';
-import { View } from 'react-native';
-import AddEntry from './components/AddEntry'
+import React from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableHighlight,
+  TouchableNativeFeedback,
+  TouchableOpacity,
+  TouchableWithoutFeedback
+} from "react-native";
+import AddEntry from "./components/AddEntry";
 
 export default function App() {
   return (
     <View>
-      <AddEntry></AddEntry>
+      <AddEntry />
     </View>
   );
 }

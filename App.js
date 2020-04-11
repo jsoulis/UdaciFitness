10365dd154282cc0ae21dbc50412b888976a4517
jsoulis <jtsoulis7@gmail.com>
2020-04-11 14:25:45 +0900

    Implemented styles for AddEntry Componenent and started building out the Calendar / History for different past entries

--- App.js
+++ App.js
@@ -1,19 +1,20 @@
 import React from "react";
-import {
-  View,
-  Text,
-  StyleSheet,
-  TouchableHighlight,
-  TouchableNativeFeedback,
-  TouchableOpacity,
-  TouchableWithoutFeedback
-} from "react-native";
+import { View } from "react-native";
 import AddEntry from "./components/AddEntry";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+import reducer from './reducers';
+import History from './components/History'
 
 export default function App() {
   return (
-    <View>
-      <AddEntry />
-    </View>
+    <Provider store={createStore(reducer)}>
+      <View style={{flex: 1}}>
+        <View style={{height: 20}}/>
+        <History />
+      </View>
+    </Provider>
+    
   );
 }
+
